0c6d059426fe4b6ec7016ad5cf89c2b2db80b80f
ishraqe <anamul.ish@gmail.com>
2023-01-29 00:08:26 +0600

    live url added

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080/get/news";
+export const BASE_URL = "https://dead-cyan-whale-belt.cyclic.app/get/news";
 export const TABS = ["General", "Business", "Sports", "Entertainment"];
